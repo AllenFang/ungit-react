8dbd6984c34e6022dce674ad0598859d0238f7b9
AllenFang <ayu780129@hotmail.com>
2017-06-04 21:23:28 +0800

    enhance error handling and also handle UI staus when API is calling

--- src-react/actions/ungit-config.js
+++ src-react/actions/ungit-config.js
@@ -1,12 +1,17 @@
 import * as types from 'constants/action-types';
+
 export function fetchUngitConfig() {
   return dispatch => {
+    dispatch(pending());
     // consider wrap API call in separate modules
     // it will be easy to stub module's function when testing
     fetch('http://localhost:8448/ungit/config')
       .then(response => response.json())
       .then(json => {
         dispatch(receiveUgitConfig(json));
+      })
+      .catch(e => {
+        dispatch(apiError(e.message));
       });
   };
 };
@@ -14,6 +19,19 @@ export function fetchUngitConfig() {
 export function receiveUgitConfig(ungitConfig) {
   return {
     type: types.RECEIVE_UNGIT_CONFIG,
-    ungitConfig
+    payload: ungitConfig
   };
 };
+
+export function pending() {
+  return {
+    type: types.PATH_PAGE_PENDING
+  };
+};
+
+export function apiError(message) {
+  return {
+    type: types.PATH_PAGE_API_ERR,
+    payload: message
+  };
+}
