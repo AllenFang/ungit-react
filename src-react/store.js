071f30aa4229769b7c21b2920e4defb3bc00c248
AllenFang <ayu780129@hotmail.com>
2017-06-04 17:52:12 +0800

    configure redux-thunk

--- src-react/store.js
+++ src-react/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import ungitApp from './reducers';
 
-const store = createStore(ungitApp);
+const store = createStore(ungitApp, applyMiddleware(thunk));
 
 export default store;
