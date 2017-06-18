86d55e9a1c7078417c28cf5fdca5f561dac1f969
AllenFang <ayu780129@hotmail.com>
2017-06-18 18:34:18 +0800

    use redux-fetch-middleware and configure middleware

--- src-react/store.js
+++ src-react/store.js
@@ -1,6 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import fetchMiddlewareCreator from 'redux-fetch-middleware';
 import ungitApp from './reducers';
+
+const fetchMiddleware = fetchMiddlewareCreator({
+  suffix: ['REQUEST', 'SUCCESS', 'FAILURE'],
+  debug: process.env.NODE_ENV === 'development',
+  fetchOptions: {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+});
+
+const middlewares = [thunk, fetchMiddleware];
 
 const initialState = {
   config: {
@@ -23,6 +37,6 @@ const initialState = {
   }
 };
 
-const store = createStore(ungitApp, initialState, applyMiddleware(thunk));
+const store = createStore(ungitApp, initialState, applyMiddleware(...middlewares));
 
 export default store;
